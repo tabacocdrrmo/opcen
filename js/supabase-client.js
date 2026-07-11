f9edc245d03b2c351bd1ee6a305c5341e4e534f2
TephHereToCode <josephriosa.dev@gmail.com>
2026-07-11 15:34:12 +0800

    refactor: replace supabase config with client

---
+++ js/supabase-client.js
@@ -0,0 +1,5 @@
+const { createClient } = supabase;
+const supabaseClient = createClient(
+    "https://lwlpftfaxtvhdvmxcvtm.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3bHBmdGZheHR2aGR2bXhjdnRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM0Nzk5NzksImV4cCI6MjA5OTA1NTk3OX0.gAv_Yh3n-y6KlWq1kpa5XFojrQfqRDQnutv1t8IwU1U"
+);
